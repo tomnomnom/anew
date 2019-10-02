403cf1ddef40898ee28ccf8830ba81ede0ba2c56
Ice3man543 <nizamulrana@gmail.com>
2019-10-02 18:48:36 +0530

    Used struct instead of bool map, changed the logic to make it more efficient

--- main.go
+++ main.go
@@ -16,39 +16,40 @@ func main() {
 		os.Exit(1)
 	}
 
-	lines := make(map[string]bool)
+	lines := make(map[string]struct{})
 
-	// read the whole file into a map if it exists
-	f, err := os.Open(fn)
-	if err == nil {
-		sc := bufio.NewScanner(f)
-
-		for sc.Scan() {
-			lines[sc.Text()] = true
+	// Read the input to append to a map. This is done since the
+	// data to be appended would be less than data to which
+	// it is to be appended. This saves memory.
+	sc := bufio.NewScanner(os.Stdin)
+	for sc.Scan() {
+		line := sc.Text()
+		if _, ok := lines[line]; !ok {
+			lines[line] = struct{}{}
 		}
-		f.Close()
 	}
 
-	// re-open the file for appending new stuff
-	f, err = os.OpenFile(fn, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	// Re-Open the file in append mode. Read the lines and if any of them
+	// match with the line in our map, remove it from the map.
+	f, err := os.OpenFile(fn, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to open file for writing: %s\n", err)
 	}
-	defer f.Close()
-
-	// read the lines, append and output them if they're new
-	sc := bufio.NewScanner(os.Stdin)
+	sc = bufio.NewScanner(f)
 
 	for sc.Scan() {
 		line := sc.Text()
-		if lines[line] {
+		if _, ok := lines[line]; ok {
+			delete(lines, line)
 			continue
 		}
+	}
 
-		// add the line to the map so we don't get any duplicates from stdin
-		lines[line] = true
-
+	// Finally write all the remaining elements in input map to the output
+	// as they now contain only unique words.
+	for line := range lines {
 		fmt.Println(line)
 		f.WriteString(line + "\n")
 	}
+	f.Close()
 }
