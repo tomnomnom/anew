d09dad0ce98f11887f3d75df55bccbcda43f7b96
Tom Hudson <mail@tomnomnom.com>
2019-12-05 12:26:51 +0000

    Ignores the case with no filename so you can just write to stdout with no dupes

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -11,30 +12,31 @@ func main() {
 	flag.Parse()
 
 	fn := flag.Arg(0)
-	if fn == "" {
-		fmt.Println("usage: anew <file>")
-		os.Exit(1)
-	}
 
 	lines := make(map[string]bool)
 
-	// read the whole file into a map if it exists
-	f, err := os.Open(fn)
-	if err == nil {
-		sc := bufio.NewScanner(f)
+	var f io.WriteCloser
 
-		for sc.Scan() {
-			lines[sc.Text()] = true
+	if fn != "" {
+		// read the whole file into a map if it exists
+		r, err := os.Open(fn)
+		if err == nil {
+			sc := bufio.NewScanner(r)
+
+			for sc.Scan() {
+				lines[sc.Text()] = true
+			}
+			r.Close()
 		}
-		f.Close()
-	}
 
-	// re-open the file for appending new stuff
-	f, err = os.OpenFile(fn, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to open file for writing: %s\n", err)
+		// re-open the file for appending new stuff
+		f, err = os.OpenFile(fn, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to open file for writing: %s\n", err)
+			return
+		}
+		defer f.Close()
 	}
-	defer f.Close()
 
 	// read the lines, append and output them if they're new
 	sc := bufio.NewScanner(os.Stdin)
@@ -49,6 +51,8 @@ func main() {
 		lines[line] = true
 
 		fmt.Println(line)
-		f.WriteString(line + "\n")
+		if fn != "" {
+			fmt.Fprintf(f, "%s\n", line)
+		}
 	}
 }
