e75fc87f80d8910ccd8b3e9d4dbbb07ab42740f8
Manas Bellani <edwards.aus@hotmail.com>
2020-08-18 17:39:03 +1000

    main.go: fixed bug where all lines would have been displayed when lines not read from file, but as function

--- main.go
+++ main.go
@@ -24,10 +24,11 @@ func appendDisplayLine(f io.WriteCloser, fn string, line string, lines map[strin
 // Anew - A public function which will append new lines `nl` provided as input
 // to file `fn`. If quietMode `quietMode` enabled, then do not print lines to
 // stdout. If `readFromStdin` is set to true, then lines to append will be read
-// from stdin instead of `nl`
-func Anew(nl []string, fn string, quietMode bool, readFromStdin bool) {
+// from stdin instead of `nl`. Returns newlines that are found.
+func Anew(nl []string, fn string, quietMode bool, readFromStdin bool) []string {
 	lines := make(map[string]bool)
 
+	var newlines []string
 	var f io.WriteCloser
 
 	if fn != "" {
@@ -46,7 +47,7 @@ func Anew(nl []string, fn string, quietMode bool, readFromStdin bool) {
 		f, err = os.OpenFile(fn, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "failed to open file for writing: %s\n", err)
-			return
+			return newlines
 		}
 		defer f.Close()
 	}
@@ -59,16 +60,21 @@ func Anew(nl []string, fn string, quietMode bool, readFromStdin bool) {
 			if lines[l] {
 				continue
 			}
-
+			newlines = append(newlines, l)
 			appendDisplayLine(f, fn, l, lines, quietMode)
 		}
 	} else {
 
 		for l := range lines {
+			if lines[l] {
+				continue
+			}
 			appendDisplayLine(f, fn, l, lines, quietMode)
+			newlines = append(newlines, l)
 		}
 
 	}
+	return newlines
 }
 
 func main() {
